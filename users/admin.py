0320e01d85b4c9fb80b84f8370e5aaa2c8190d87
katekatics <E.Astrakhantseva@x5.ru>
2020-12-02 21:47:40 +0300

    admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+class RegularUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        'username', 'email', 'phone_number', 'age', 'salary_amount', 'is_staff'
+    ]
+
+
+admin.site.register(CustomUser, RegularUserAdmin)
